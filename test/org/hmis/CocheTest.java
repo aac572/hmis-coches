2144e4539235b45e4d72d21e8e4567aaa8f7cc7c
Alberto Arroyo Cárdenas <albertoarroyocardenas@gmail.com>
2023-05-22 16:49:02 +0200

    terminado

--- test/org/hmis/CocheTest.java
+++ test/org/hmis/CocheTest.java
@@ -3,7 +3,6 @@ package org.hmis;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
--- test/org/hmis/JsonReaderTest.java
+++ test/org/hmis/JsonReaderTest.java
@@ -3,11 +3,6 @@ package org.hmis;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.FileNotFoundException;
-
-import javax.management.RuntimeErrorException;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
