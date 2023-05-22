887fcec1c72b3722811305b0be0fe731bf725a44
Alberto Arroyo Cárdenas <albertoarroyocardenas@gmail.com>
2023-05-22 16:39:55 +0200

    hmis-coches terminado

--- test/org/hmis/CocheTest.java
+++ test/org/hmis/CocheTest.java
@@ -4,90 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class CocheTest {
 
-   
-    @Test
-    void testEquals() {
-        Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-        Coche segundo = new Coche ("Renault", "Clio", 2001, 12000);
-        assertEquals(primero, segundo, "Los dos coches no son iguales");
+    private static Coche[] coches() {
+        String ruta = "data/coches.json";
+		Coche [] coches = JsonReader.leerCochesJSON(ruta);
+        return coches;
     }
 
-    @Test
-    void testToString() {
-        Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-        assertEquals("Coche [marca=Renault, modelo=Clio, año=2001, precio=12000]", primero.toString());
-
+    @ParameterizedTest
+    @MethodSource("coches")
+    void testEquals(Coche coche) {
+        Coche coche2= new Coche();
+        coche2.setAño(coche.getAño()+1);
+        assertEquals(coche, coche, "Los coches deberían ser iguales");
+        assertNotEquals(coche, coche2, "Los coches no deberían ser iguales");
     }
 
-    // Test del metodo equals de la clase Coche
-    @Test
-    void testEquals2() {
-        Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-        Coche segundo = null;
-        assertNotEquals(primero, segundo, "Los dos coches no son iguales");
+    @ParameterizedTest
+    @MethodSource("coches")
+    void testToString(Coche coche) {
+        String expected = "Coche [marca=" + coche.getMarca() + ", modelo=" + coche.getModelo() + ", año=" + coche.getAño() + ", precio=" + coche.getPrecio() + "]";
+        assertEquals(expected, coche.toString());
+
     }
     
-    // Test del metodo equals de la clase Coche
-    @Test
-    void testEquals3() {
-        Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-        assertEquals(primero, primero, "Los dos coches no son iguales");
+    // test getters
+    @ParameterizedTest
+    @CsvSource({"Seat, Ibiza, 2010, 4000"})
+    void testGetters(String marca, String modelo, String year, String precio) {
+        Coche coche = new Coche();
+        coche.setMarca(marca);
+        coche.setModelo(modelo);
+        coche.setAño(Integer.valueOf(year));
+        coche.setPrecio(Integer.valueOf(precio));
+        assertEquals(marca, coche.getMarca());
+        assertEquals(modelo, coche.getModelo());
+        assertEquals(year, String.valueOf(coche.getAño()));
+        assertEquals(precio, String.valueOf(coche.getPrecio()));
     }
-
-        // Test del método equals de la clase Coche
-        @Test
-        void testEquals4() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            String segundo = "";
-            assertNotEquals(primero, segundo, "Los dos coches no son iguales");
-        }
-
-        @Test
-        void testEquals_att1() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            Coche segundo = new Coche ("Renault", "Clio", 2001, 12000);
-
-            segundo.setAño(2003);
-            assertNotEquals(primero, segundo, "Los dos coches no son iguales");
-        }
-
-        @Test
-        void testEquals_att2() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            Coche segundo = new Coche ("Renault", "Clio", 2001, 12000);
-
-            segundo.setMarca("Seat");
-            assertNotEquals(primero, segundo, "Los dos coches no son iguales");
-        }
-
-        @Test
-        void testEquals_att3() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            Coche segundo = new Coche ("Renault", "Clio", 2001, 12000);
-            segundo.setModelo("Twingo");
-            assertNotEquals(primero, segundo, "Los dos coches no son iguales");
-        }
-
-        @Test
-        void testEquals_att4() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            Coche segundo = new Coche ("Renault", "Clio", 2001, 12000);
-            segundo.setPrecio(16000);
-            assertNotEquals(primero, segundo, "Los dos coches no son iguales");
-        }
-
-        // test getters
-        @Test
-        void testGetters() {
-            Coche primero = new Coche ("Renault", "Clio", 2001, 12000);
-            assertEquals("Renault", primero.getMarca());
-            assertEquals("Clio", primero.getModelo());
-            assertEquals(2001, primero.getAño());
-            assertEquals(12000, primero.getPrecio());
-        }
 
 
 }
--- test/org/hmis/JsonReaderTest.java
+++ test/org/hmis/JsonReaderTest.java
@@ -8,19 +8,21 @@ import java.io.FileNotFoundException;
 import javax.management.RuntimeErrorException;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class JsonReaderTest {
 
-	@Test
-	void testLeerCochesJSON() {
-		String ruta = "data/coches.json";
+	@ParameterizedTest
+	@CsvSource({"data/coches.json"})
+	void testLeerCochesJSON(String ruta) {
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
 		assertEquals (4, coches.length);
 	}
 
-	@Test
-	void testLeerCochesJSONprimero() {
-		String ruta = "data/coches.json";
+	@ParameterizedTest
+	@CsvSource({"data/coches.json"})
+	void testLeerCochesJSONprimero(String ruta) {
 		Coche primero = new Coche ("Toyota", "Corolla", 2022, 22000);
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
 		assertEquals(primero, coches[0]);
@@ -28,18 +30,18 @@ class JsonReaderTest {
 		assertTrue (coches[0].equals(primero));
 	}
 
-	@Test
-	void testLeerCochesException() {
-		String ruta = "data/coches.json";
+	@ParameterizedTest
+	@CsvSource({"data/coches.json"})
+	void testLeerCochesException(String ruta) {
 		Coche [] coches = JsonReader.leerCochesJSON(ruta);
 		assertThrows(ArrayIndexOutOfBoundsException.class, () -> coches[4].equals(null));
 	}
 
-	@Test
-	void testLeerArchivoNoExistente() {
+	@ParameterizedTest
+	@CsvSource({"data/cochesNoExistente.json"})
+	void testLeerArchivoNoExistente(String ruta) {
 		JsonReader jsonReader = new JsonReader();
-		assertEquals(JsonReader.class, jsonReader.getClass(), "jsoneader es del tipo JsonReader");
-		String ruta = "data/esteArchivoNoExiste.exception";
+		assertEquals(JsonReader.class, jsonReader.getClass(), "jsonReader es del tipo JsonReader");
 		try {
 			JsonReader.leerCochesJSON(ruta);
 		} catch (Exception e){
